4429b2a008786f12e6e52fe3822913bf93212629
papasushi <papasushi@gmail.com>
2019-07-02 09:10:13 +0100

    first commit

---
+++ if-statements.py
@@ -0,0 +1,34 @@
+number=5
+if number:
+    print("number is defined and truthy")
+
+text = "Python"
+if text:
+    print("test is defined and truthy")
+
+python_course = True
+if python_course:
+    print("this will execute")
+
+aliens_found = None
+if aliens_found:
+    print("this will NOT execute")
+
+number=5
+if number != 5:
+    print("this will not execute")
+
+python_course = True
+if not python_course:
+    print("this will also not execute")
+
+number = 3
+python_course=True
+if number == 3 and python_course:
+    print("this will execute")
+if number == 17 or python_course:
+    print ("this will also execute")
+
+a=1
+b=2
+print ("bigger" if a > b else "smaller")
