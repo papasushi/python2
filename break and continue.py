bee04980413331da847288836a6c8bf294fdbec0
papasushi <papasushi@gmail.com>
2019-07-05 09:53:09 +0100

    fifth commit

---
+++ break and continue.py
@@ -0,0 +1,17 @@
+student_names = ["James","Katrina","Jessica","Mark","Bort","Frank Grimes","Max Power"]
+
+for name in student_names:
+    if name =="Mark":
+        print("found him! " + name)
+        break
+    print ("Currently testing " + name)
+
+
+student_names = ["James","Katrina","Jessica","Mark","Bort","Frank Grimes","Max Power"]
+
+for name in student_names:
+    if name =="Bort":
+        continue
+        print("found him! " + name)
+        break
+    print ("Currently testing " + name)
---
+++ printing list elements.py
@@ -0,0 +1,12 @@
+student_names = ["Mark","Katrina","Jessica","James","Bort","Frank Grimes","Max Power"]
+for name in student_names:
+    print('Student name is {0}'.format(name))
+
+x=0
+for index in range(11,14):
+    x += 10
+    print("The value of X is {0}".format(x))
+
+
+
+
