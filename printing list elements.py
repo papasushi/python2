15558dad432eedd35251870f5cfb636a0d5744bf
papasushi <papasushi@gmail.com>
2019-07-09 08:45:04 +0100

    on annual leave push

--- printing list elements.py
+++ printing list elements.py
@@ -3,7 +3,7 @@ for name in student_names:
     print('Student name is {0}'.format(name))
 
 x=0
-for index in range(11,14):
+for index in range(0, 4):
     x += 10
     print("The value of X is {0}".format(x))
 
